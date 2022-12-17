96268c78f095c5c60a767e6beda96702e51be466
Aromakc <suwashkhatry38@gmail.com>
2022-12-17 19:37:56 +0545

    DDA and Bresenham Done

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,39 @@
 #include"shader.h"
 #include"VAO.h"
 #include"VBO.h"
-#include"EBO.h"
 
-float Normalize(float input, float min, float max)
+float NormalizeCoordinate(float input, float min, float max)
 {
-	return (input - min) / (max - min);
+	//normalize the coordiante of the viewport between 0 and 1
+	return (input - min) / (max - min); 
 }
-void DrawPixel(int posX, int posY)
+void setPixel(float posX, float posY, float k)
 {
-	//float vertices[] = {
-	//Normalize(posX, 0, 800), Normalize(posY, 0, 800),0.0f
-	//};
-	////Drawing The initial coordinate
-	//VAO VAO1;
-	//VAO1.Bind();
+	float vertices[] = { NormalizeCoordinate(posX, 0, 800), NormalizeCoordinate(posY, 0, 800), 0.0f, k * 0.1, 0, 0 };
 
-	//VBO VBO1(vertices, sizeof(vertices));
-	//VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
-	//
-	////VAO1.Unbind();
-	////VBO1.Unbind();
+	VAO VAO1;
+	VAO1.Bind();
 
-	//glPointSize(15);
-	//glDrawArrays(GL_POINTS, 0, 1);
-	//VAO1.Unbind();
-	//VAO1.Delete();
-	//VBO1.Delete();
+	VBO VBO1(vertices, sizeof(vertices));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	
+	VAO1.Unbind();
+	VBO1.Unbind();
 
-	glBegin(GL_POINTS);
-	glVertex2i(posX, posY);
-	glEnd();
-	glFlush();
+	VAO1.Bind();
+	glPointSize(5); //specify the diameter of rasterized points
+	glDrawArrays(GL_POINTS, 0, 1); // point as primitive, start, size
+
+	VAO1.Unbind();
+	VAO1.Delete();
+	VBO1.Delete();	
 }
 
-void DDADisplayLine(int xi, int yi, int xj, int yj)
+// Algorithm of Digital Differential Line Drawing 
+void DDA_LDA(float xi, float yi, float xj, float yj)
 {
-	int dy, dx, steps, x, y, k;
+	float dy, dx, steps, x, y, k;
 	float Xinc, Yinc;
 	dx = xj - xi;
 	dy = yj - yi;
@@ -48,16 +45,80 @@ void DDADisplayLine(int xi, int yi, int xj, int yj)
 		steps = abs(dy);
 	else
 		steps = abs(dy);
-	Xinc = (float) (dx / steps);
-	Yinc = (float) (dy / steps);
+	Xinc = dx / steps;
+	Yinc = dy / steps;
 	x = xi;
 	y = yi;
-	DrawPixel(x, y);
+	setPixel(x, y,0);
 	for (k = 1; k <= steps; k++)
 	{
 		x += Xinc;
 		y += Yinc;
-		DrawPixel(round(x), round(y));
+		setPixel(round(x), round(y),0);
+	}
+}
+
+// Algorithm of Bresenham Line Drawing
+void Bresenham_LDA(float xi, float yi, float xj, float yj)
+{
+	float dy, dx, x,y;
+	dx = xj - xi;
+	dy = yj - yi;
+
+
+	if (abs(dy) < abs(dx)) // |m| < 1.0
+	{
+		float p = 2 * dy - dx;
+
+		//Determine which endpoint to start as start position
+		if (xi > xj) {
+			x = xj;
+			y = yj;
+			xj = xi;
+		}
+		else {
+			x = xi;
+			y = yi;
+		}
+		setPixel(x, y, 0);
+		while (x < xj) {
+			x++;
+			if (p < 0) {
+				p += (2 * dy);
+			}
+			else {
+				y++;
+				p += (2 * (dy - dx));
+			}
+			setPixel(x, y, 0);
+		}
+	}
+	else // |m| > 1.0
+	{
+		float p = 2 * dx - dy;
+
+		//Determine which endpoint to start as start position
+		if (yi > yj) {
+			x = xj;
+			y = yj;
+			yj = yi;
+		}
+		else {
+			x = xi;
+			y = yi;
+		}
+		setPixel(x, y, 0);
+		while (y < yj) {
+			y++;
+			if (p < 0) {
+				p += (2 * dx);
+			}
+			else {
+				x++;
+				p += (2 * (dx - dy));
+			}
+			setPixel(x, y, 0);
+		}
 	}
 }
 
@@ -83,10 +144,9 @@ int main()
 	gladLoadGL();
 	glViewport(0, 0, 800, 800);
 
-	//Shader shaderProgram("resources/Shader/vertexShader.vs", "resources/Shader/fragmentShader.vs");
+	Shader shaderProgram("resources/Shader/vertexShader.vs", "resources/Shader/fragmentShader.fs");
 
-	const int pixelSize = 15;
-	int xi, xj, yi, yj;
+	float xi, xj, yi, yj;
 	std::cout << "Enter starting coordinate xi yi : ";
 	std::cin >> xi >> yi;
 	std::cout << "Enter ending coordinate xj yj : ";
@@ -94,14 +154,18 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		//shaderProgram.use();
-		DDADisplayLine(xi, yi, xj, yj);
+
+		shaderProgram.use();
+
+		//DDA_LDA(xi, yi, xj, yj);
+		Bresenham_LDA(xi, yi, xj, yj); //function to draw line
+		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	//shaderProgram.~Shader();
+	shaderProgram.~Shader();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
