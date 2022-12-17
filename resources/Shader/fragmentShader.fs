c93a95906fa6fcc7f32d3dda804873399974d3cb
Aromakc <suwashkhatry38@gmail.com>
2022-12-17 13:44:06 +0545

    LDA unfinished

--- resources/Shader/fragmentShader.fs
+++ resources/Shader/fragmentShader.fs
@@ -1,12 +1,7 @@
 #version 330 core
 out vec4 FragColor;
-in vec3 ourColor;
-in vec2 texCoord;
-
-uniform sampler2D tex0;	
 
 void main()
 {
-   //FragColor = vec4(ourColor, 1.0f);
-   FragColor = texture(tex0, texCoord);
+   FragColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,65 @@
-//------- Ignore this ----------
-//#include<filesystem>
-//namespace fs = filesystem;
-//------------------------------
-
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
-#include<stb/stb_image.h>
 
-#include"texture.h"
 #include"shader.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
 
-
-float vertices[] =
-{ 
-	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
-	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
-	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
-	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
-};
-
-unsigned int indices[] =
+float Normalize(float input, float min, float max)
 {
-	0, 2, 1, // Upper triangle
-	0, 3, 2 // Lower triangle
-};
+	return (input - min) / (max - min);
+}
+void DrawPixel(int posX, int posY)
+{
+	//float vertices[] = {
+	//Normalize(posX, 0, 800), Normalize(posY, 0, 800),0.0f
+	//};
+	////Drawing The initial coordinate
+	//VAO VAO1;
+	//VAO1.Bind();
 
+	//VBO VBO1(vertices, sizeof(vertices));
+	//VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
+	//
+	////VAO1.Unbind();
+	////VBO1.Unbind();
+
+	//glPointSize(15);
+	//glDrawArrays(GL_POINTS, 0, 1);
+	//VAO1.Unbind();
+	//VAO1.Delete();
+	//VBO1.Delete();
+
+	glBegin(GL_POINTS);
+	glVertex2i(posX, posY);
+	glEnd();
+	glFlush();
+}
+
+void DDADisplayLine(int xi, int yi, int xj, int yj)
+{
+	int dy, dx, steps, x, y, k;
+	float Xinc, Yinc;
+	dx = xj - xi;
+	dy = yj - yi;
+	if (abs(dy) > abs(dx))
+		steps = abs(dy);
+	else
+		steps = abs(dy);
+	Xinc = (float) (dx / steps);
+	Yinc = (float) (dy / steps);
+	x = xi;
+	y = yi;
+	DrawPixel(x, y);
+	for (k = 1; k <= steps; k++)
+	{
+		x += Xinc;
+		y += Yinc;
+		DrawPixel(round(x), round(y));
+	}
+}
 
 
 int main()
@@ -52,56 +83,25 @@ int main()
 	gladLoadGL();
 	glViewport(0, 0, 800, 800);
 
-	Shader shaderProgram("../resources/Shader/vertexShader.vs", "../resources/Shader/fragmentShader.vs");
+	//Shader shaderProgram("resources/Shader/vertexShader.vs", "resources/Shader/fragmentShader.vs");
 
-	VAO VAO1;
-	VAO1.Bind();
-
-	VBO VBO1(vertices, sizeof(vertices));
-	EBO EBO1(indices, sizeof(indices));
-
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-
-	VAO1.Unbind();
-	VBO1.Unbind();
-	EBO1.Unbind();
-
-	unsigned int uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-
-	/*std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
-	std::string texPath = "../resources/Textures/";
-
-	 Texture
-	Texture popCat((parentDir + texPath + "pop_cat.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	popCat.texUnit(shaderProgram, "tex0", 0);*/
-
-	// Original code from the tutorial
-	Texture popCat("../resources/Textures/pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	popCat.texUnit(shaderProgram, "tex0", 0);
+	const int pixelSize = 15;
+	int xi, xj, yi, yj;
+	std::cout << "Enter starting coordinate xi yi : ";
+	std::cin >> xi >> yi;
+	std::cout << "Enter ending coordinate xj yj : ";
+	std::cin >> xj >> yj;
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		
-		shaderProgram.use();
-		glUniform1f(uniID, 0.5f);
-		// Binds texture so that is appears in rendeng
-		popCat.Bind();
-		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		
+		//shaderProgram.use();
+		DDADisplayLine(xi, yi, xj, yj);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
-	VAO1.Delete();
-	VBO1.Delete();
-	EBO1.Delete();
-	popCat.Delete();
-	shaderProgram.~Shader();
+	//shaderProgram.~Shader();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
